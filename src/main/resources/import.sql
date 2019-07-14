724d09c57ed1bc0a69eb9bf3ffa8e68b0f29fffb
Thiago Bianeck <thiagobianeck@gmail.com>
2019-07-14 20:24:07 -0300

    Criando a view listar

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Thiago', 'Bianeck', 'thiagobianeck@gmail.com', '2019-07-14');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'Leon', 'Bianeck', 'leonmaicabianeck@gmail.com', '2019-07-14');
